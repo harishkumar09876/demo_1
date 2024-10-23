1d422c054007f5f34934602cbc999dc604b6cec2
harishkumar09876 <rajannaharish680@gmail.com>
2024-10-23 17:58:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  region     = us-east-1
-  access_key = AKIATJHQD6OKL4DSXK46
-  secret_key = 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
+  region     = "us-east-1"
+  access_key = "AKIATJHQD6OKL4DSXK46"
+  secret_key = "WnR5TT30YHKT+L5FfREqm46o4sbyHk38nC4UhO1M"
 }
  
 resource "aws_instance" "example" {
