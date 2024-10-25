356b68d645f5dc9582d1b41328db2640ce4118a1
harishkumar09876 <harish@SYNBLRLT3045NRQ.synechron.com>
2024-10-25 13:30:48 +0530

    commit done 1

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,8 +1,8 @@
 def call(String repoUrl, String branch = 'master') {
     checkout([
         $class: 'GitSCM',
-        branches: [[name: "*/${master}"]],
-        userRemoteConfigs:  [[url: "${https://github.com/harishkumar09876/demo_1.git}"]]
+        branches: [[name: "*/${branch}"]],
+        userRemoteConfigs:  [[url: "${repoUrl}"]]
     ])
 }
 
