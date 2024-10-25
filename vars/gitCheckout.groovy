5bdf23d3b35eaa2d1baab1d5a93647e7c6ec051b
harishkumar09876 <harish@SYNBLRLT3045NRQ.synechron.com>
2024-10-25 13:11:17 +0530

    latest commit 1

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,3 +1,8 @@
-def call {
-checkout scmGit(branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[credentialsId: 'github', url: 'https://github.com/harishkumar09876/demo_1.git']])
+def call(String repoUrl, String branch = 'master') {
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name: "*/${master}"]],
+        userRemoteConfigs: [[url: https://github.com/harishkumar09876/demo_1.git]]
+    ])
 }
+
