f324e168978ffde6a4245f595939cf0f48b6f5b7
harishkumar09876 <harish@SYNBLRLT3045NRQ.synechron.com>
2024-10-25 14:06:42 +0530

    done done

--- vars/postBuildActions.groovy
+++ vars/postBuildActions.groovy
@@ -1,19 +1,9 @@
 def call() {
-    post {
-        success {
-            mail to: 'harishkumarbsr006@gmail.com',
-                subject: "SUCCESS: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
-                body: "Good news, the job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' has succeeded."
-        }
-        failure {
-            mail to: 'harishkumarbsr006@gmail.com',
-                subject: "FAILURE: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
-                body: "Unfortunately, the job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' has failed."
-        }
-        always {
-            cleanWs()
-        }
-    }
+    // No need for `post` block here
+    mail to: 'harishkumarbsr006@gmail.com',
+         subject: "Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' Status",
+         body: "The job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' has ${currentBuild.currentResult.toLowerCase()}."
+    cleanWs()
 }
 
 
