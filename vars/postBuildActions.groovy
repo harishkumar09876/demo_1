cdbf2a676c049c2c36fa74e9ecd08cd33db23c31
harishkumar09876 <harish@SYNBLRLT3045NRQ.synechron.com>
2024-10-28 23:25:24 +0530

    updated post

--- vars/postBuildActions.groovy
+++ vars/postBuildActions.groovy
@@ -1,9 +1,19 @@
-def call() {
-    // No need for `post` block here
-    mail to: 'harishkumarbsr006@gmail.com',
-         subject: "Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' Status",
-         body: "The job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' has ${currentBuild.currentResult.toLowerCase()}."
+def call(String alertTo) {
+    // Assuming alertTo can be 'email' or 'teams'
+    if (alertTo == 'email') {
+        mail to: 'harishkumarbsr006@gmail.com',
+             subject: "Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' Status",
+             body: "The job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' has ${currentBuild.currentResult.toLowerCase()}."
+    } else if (alertTo == 'teams') {
+        // Teams alert logic (assume you have a method to send Teams alerts)
+        sendTeamsAlert("Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' has ${currentBuild.currentResult.toLowerCase()}.")
+    }
     cleanWs()
+}
+
+def sendTeamsAlert(String message) {
+    // Method to send alerts to Microsoft Teams
+    echo "Sending to Teams: ${message}"
 }
 
 
