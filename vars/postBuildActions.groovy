785046f11a0cd62ca71326c3de44a9888dacce8f
harishkumar09876 <harish@SYNBLRLT3045NRQ.synechron.com>
2024-11-05 11:31:31 +0530

    committed

--- vars/postBuildActions.groovy
+++ vars/postBuildActions.groovy
@@ -1,19 +1,19 @@
 def call(String alertTo) {
-    // Assuming alertTo can be 'email' or 'teams'
+    
     if (alertTo == 'email') {
-        mail to: 'harishkumarbsr006@gmail.com',
+        mail to:  "${env.EMAIL}",
              subject: "Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' Status",
              body: "The job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' has ${currentBuild.currentResult.toLowerCase()}."
     } else if (alertTo == 'teams') {
-        // Teams alert logic (assume you have a method to send Teams alerts)
+        
         sendTeamsAlert("Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' has ${currentBuild.currentResult.toLowerCase()}.")
     }
     cleanWs()
 }
 
 def sendTeamsAlert(String message) {
-    // Method to send alerts to Microsoft Teams
+   
     echo "Sending to Teams: ${message}"
 }
-
+ 
 
